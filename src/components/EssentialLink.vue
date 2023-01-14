df148ec1534a9200321b7d5dd7059d7657cc22f6
Yurec <yurecblinovgelarm2gmail.com>
2023-01-14 17:00:14 +0300

    разбивка по компонентам

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,48 +1,83 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
+  <q-btn color="primary"
+    v-for="item in btns"
+    :key="item"
   >
-    <q-item-section
-      v-if="icon"
-      avatar
+  <div v-html="item.component"></div>
+    <q-menu
+      anchor="top right"
+      self="top left"
     >
-      <q-icon :name="icon" />
-    </q-item-section>
-
-    <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>{{ caption }}</q-item-label>
-    </q-item-section>
-  </q-item>
+      <q-list style="min-width: 100px">
+        <q-item clickable v-close-popup>
+          <q-item-section>anchor="top right"</q-item-section>
+        </q-item>
+        <q-item clickable v-close-popup>
+          <q-item-section>self="top left"</q-item-section>
+        </q-item>
+      </q-list>
+    </q-menu>
+  </q-btn>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { ref, defineComponent } from 'vue'
+import comp from 'components/comp'
 
 export default defineComponent({
   name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    icon: {
-      type: String,
-      default: ''
+  components: {
+    comp
+  },
+  setup() {
+    const btns = ref([
+      {
+        title: '',
+        icon: 'cursor',
+      },
+      {
+        title: '',
+        icon: 'line'
+      },
+      {
+        title: '',
+        icon: 'circleline'
+      },
+      {
+        title: '',
+        icon: 'pencil'
+      },
+      {
+        title: '',
+        icon: 'rectangle'
+      },
+      {
+        title: '',
+        icon: 'circle'
+      },
+      {
+        title: '',
+        icon: 'pointer'
+      },
+      {
+        title: '',
+        icon: 'back'
+      },
+      {
+        title: '',
+        icon: 'pouring'
+      },
+      {
+        title: '',
+        icon: 'settings'
+      },
+      {
+        title: '',
+        icon: 'close'
+      },
+    ])
+    return {
+      btns
     }
   }
 })
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,70 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="hHh lpR fFf">
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left">
+      <EssentialLink />
+    </q-drawer>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right">
+      <q-btn 
+        outline style="color: grey;" 
+        label="Назад в калькулятор" 
+        class="btn-back"
+      />
+      <div class="section-btn">
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      </div>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="bg-dark text-white my-footer">
+      <q-toolbar>
+        <q-btn 
+          style="background: goldenrod;" 
+        >
+          <q-icon
+            color="grey"
+            size="18px"
+            name="svguse:icons/allIcons.svg#mail"
+          />
+          <span class="block text-grey">онлайн помощник</span>
+        </q-btn>
+        <q-btn 
+          outline style="color: white;" 
+          label="Как работать с калькулятором?" 
+        />
+      </q-toolbar>
+    </q-footer>
+
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-export default defineComponent({
-  name: 'MainLayout',
-
+export default {
   components: {
     EssentialLink
   },
-
   setup () {
     const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
       }
     }
   }
-})
+}
 </script>
